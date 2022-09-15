1f4a94d161134da07e5bf5b306f97038c5809ade
Aya Seidemann <aya.seidemann@gmail.com>
2022-09-15 14:54:22 -0700

    build server boilerplate and basic structure

---
+++ controllers/gameControllers.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+function getGame() {
+
+}
+
+function getSolution() {
+
+}
+
+module.exports = {
+    getGame,
+    getSolution
+}
---
+++ routes/gameRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const { getGame, getSolution } = require('../controllers/gameControllers');
+
+// get game based on difficulty
+router.get('/:difficulty', getGame);
+
+// get solution
+router.get('/solution', getSolution);
+
+module.exports = router;
