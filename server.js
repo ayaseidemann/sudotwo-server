5e7ff0d2f0ca9268007a16d76f3ff4fe6f1a3b6a
Aya Seidemann <aya.seidemann@gmail.com>
2022-09-20 16:09:11 -0700

    initialize socket and get solution pulling from API

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const server = http.createServer(app);
+// const { Server } = require("socket.io");
+// const io = new Server(server);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: ['http://localhost:3000'] 
+    }
+});
+
 const cors = require('cors');
 require('dotenv').config();
 const { PORT } = process.env;
 const gameRoutes = require('./routes/gameRoutes');
+
+
+// connect socket.io
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('tile-change', value => {
+        console.log('tile change to: ', value);
+    })
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
 
 // set up cors
 app.use(cors());
@@ -14,11 +36,10 @@ app.use(express.json());
 // set game route
 app.use('/', gameRoutes);
 
-// app.get('/', (req, res) => {
-//     res.send('You are in the server woohoo!');
-// });
+app.get('/', (req, res) => {
+    res.send('You are in the server woohoo!');
+});
 
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
 })
