9d6ceb4fe57eb0bade25be48345014638e35539d
Aya Seidemann <aya.seidemann@gmail.com>
2022-09-21 14:19:46 -0700

    get users in same room looking at same board

--- controllers/gameControllers.js
+++ controllers/gameControllers.js
@@ -1,15 +1,13 @@
 const { default: axios } = require('axios');
 const fs = require('fs');
 const { writeGameToJson, readRoomsData } = require('../models/gameModels');
-const { v4: uuidv4 } = require('uuid');
 
 
 async function setupGame(req, res) {
     try {
-        const difficulty = req.params.difficulty;
-        const {data: axiosBoard} = await axios.get(`https://sugoku.herokuapp.com/board?difficulty=${difficulty}`);
+        const roomId = req.params.roomId;
+        const {data: axiosBoard} = await axios.get(`https://sugoku.herokuapp.com/board?difficulty=easy`);
         const board = axiosBoard.board;
-        const roomId = uuidv4();
         const encodedBoard = new URLSearchParams({ board: JSON.stringify(board) })
         const {data: axiosSolution} = await axios.post(`https://sugoku.herokuapp.com/solve`, encodedBoard.toString());
         const solution = axiosSolution.solution;
@@ -34,24 +32,23 @@ async function setupGame(req, res) {
     }
 }
 
-function readSolution(req, res) {
+function readGame(req, res) {
     const roomId = req.params.roomId;
     console.log(roomId);
     readRoomsData((err, data) => {
         if (err) {
             console.log('error in reading from file');
         } else {
-            console.log('in read solution else statement');
             const rooms = JSON.parse(data);
             const activeRoom = rooms.find(room => room.roomId === roomId);
-            res.json(activeRoom.solution);
+            res.json(activeRoom);
         }
     })
 }
 
 module.exports = {
     setupGame,
-    readSolution
+    readGame
 }
 
 // function getGame(req, res) {
